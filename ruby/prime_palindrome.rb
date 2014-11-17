05185d097762e8edc478be3a2361a69f3148c94b
Ruslan Ivanenko <rivanenko@svitla.com>
2014-11-18 00:26:38 +0200

    return each palindrome on first position

--- ruby/prime_palindrome.rb
+++ ruby/prime_palindrome.rb
@@ -3,4 +3,6 @@ MAX = 1000
 # get prime numbers till MAX
 primes = (0..MAX).select { |a| a.to_s == a.to_s.reverse }
 # get palindromes from prime numbers
-primes.select { |p| ((2..MAX).to_a-[p]).none?{ |n| p % n == 0 } }
+prime_palindromes = primes.select { |p| ((2..MAX).to_a-[p]).none?{ |n| p % n == 0 } }
+
+prime_palindromes.size.times { |i| puts "#{prime_palindromes.last(prime_palindromes.size-i).join(' ')} #{prime_palindromes.first(i).join(' ')}" }
