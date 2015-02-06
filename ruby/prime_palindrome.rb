215e540d26dbde787d3f6a4b565ea64d9f289b70
Ruslan Ivanenko <rivanenko@svitla.com>
2015-02-06 12:45:21 +0200

    refactor prime palindromes

--- ruby/prime_palindrome.rb
+++ ruby/prime_palindrome.rb
@@ -1,8 +1,8 @@
 MAX = 1000
 
 # get prime numbers till MAX
-primes = (0..MAX).select { |a| a.to_s == a.to_s.reverse }
+palindromes = (0..MAX).select { |a| a.to_s == a.to_s.reverse }
 # get palindromes from prime numbers
-prime_palindromes = primes.select { |p| ((2..MAX).to_a-[p]).none?{ |n| p % n == 0 } }
+prime_palindromes = palindromes.select { |p| ((2..Math.sqrt(p)).to_a).none?{ |n| p % n == 0 } }
 
 prime_palindromes.size.times { |i| puts "#{prime_palindromes.last(prime_palindromes.size-i).join(' ')} #{prime_palindromes.first(i).join(' ')}" }
